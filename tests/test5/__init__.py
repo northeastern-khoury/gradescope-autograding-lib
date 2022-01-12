462ef914f0abbdf7c8af849f17c40a03424c5756
Tariq Sachleben <t.sachleben@northeastern.edu>
2022-01-12 16:47:23 -0500

    Patch InstructorFiles rmtree error
     + patch to file_util.InstructorFiles
     + post_test handling for test driver
     + Meta Testcase test5

--- tests/test5/__init__.py
+++ tests/test5/__init__.py
@@ -1,23 +1,35 @@
 #!/usr/bin/env python3
 
+import hashlib
+import os
 import subprocess
+from pathlib import Path
 
 import gradescope
 from gradescope import Tester
 from gradescope.file_util import ChDir, InstructorFiles, Manifest
 from gradescope.unix_util import ExecutableFile, Make
 
+def _file_hashes():
+  rvd = {}
+  for root, _, files in os.walk(gradescope.paths.PATH_CODE):
+    root = Path(root)
+    for fname in files:
+      fpath = root / fname
+      with open(fpath, 'rb') as ifp:
+        rvd[fpath] = hashlib.sha256(ifp.read()).hexdigest()
+  return rvd
 
 MAIN = "./main"
-MANIFEST = [ 'main.c' ]
+MAIN_C = 'main.c'
 NAMES = ["World", "Gradescope", "Tester", "Failure"]
 
-tester = Tester(skip_instructor_files=True)
-tester.prerequisite(ChDir('sub_dir'))
-tester.prerequisite(InstructorFiles(cleanup=True))
-tester.prerequisite(Manifest(*MANIFEST))
+HASHES = _file_hashes()
+
+tester = Tester()
+tester.prerequisite(Manifest(MAIN_C))
 tester.prerequisite(Make('all', clean_after=True))
-tester.prerequisite(ExecutableFile('main'))
+tester.prerequisite(ExecutableFile(MAIN))
 
 def _gen_vec(names):
   for name in names:
@@ -40,4 +52,15 @@ def test_echo(name):
   return (1, "Okay!")
 
 def post_test():
-  assert 
+  seen = []
+  new_hashes = _file_hashes()
+  for fname in HASHES:
+    seen.append(fname)
+    if fname not in new_hashes:
+      raise AssertionError(f"File '{fname}' deleted after Tester.exec")
+    if HASHES[fname] != new_hashes[fname]:
+      raise AssertionError(f"File '{fname}' different after Tester.exec "
+                            f"(Hash was: {HASHES[fname]}; now: {new_hashes[fname]}")
+  for fname in new_hashes:
+    if fname not in seen:
+      raise AssertionError(f"File '{fname}' not present before Tester.exec but present after")
